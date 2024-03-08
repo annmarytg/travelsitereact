06ce046c498a4a06b262bd46510144f2f9bdb524
annmarytg <annmarythayankery@gmail.com>
2024-03-08 19:30:22 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from './App';
+import { createRoot } from 'react-dom/client';
+
+
+const root = document.getElementById('root');
+const rootInstance = createRoot(root);
+rootInstance.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
